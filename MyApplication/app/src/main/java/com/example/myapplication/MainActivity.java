7b2cb828e737883c3cbc5205b6d57d212ac0f575
EvgeniyNikNik <nievni@gmail.com>
2020-09-13 00:30:28 +0300

    Yes/no

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,26 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.button6);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button test = findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.question_activity);
+                startActivity(new Intent(MainActivity.this, question.class));
             }
         });
-//        Button yes=findViewById(R.id.button_yes);
-//        yes.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                setContentView(R.layout.activity_main);
-//           }
-//            });
-//        Button back=findViewById(R.id.button_back);
-//        back.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                setContentView(R.layout.activity_main);
-//            }
-//        });
+
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/question.java
+++ MyApplication/app/src/main/java/com/example/myapplication/question.java
@@ -1,13 +1,36 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class question extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity);
+        final TextView qwest=findViewById(R.id.textView);
+        Button yes = findViewById(R.id.yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qwest.setText("Херняяяя");
+            }
+        });
+
+        Button button_back= findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(question.this, MainActivity.class));
+            }
+        });
 
     }
 }
